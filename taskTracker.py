3aa25e6650ab32af00bc46ad667547a5f1a164bc
DaviCodex <davidfvm2002@gmail.com>
2024-08-30 12:21:37 -0500

    solving the id duplicated issue

--- taskTracker.py
+++ taskTracker.py
@@ -16,7 +16,7 @@ parser=argparse.ArgumentParser(
     epilog="later"
 )
 parser.add_argument("--add", '-a', type=str, help="Add the task description")
-#parser.add_argument("--update", '-u', nargs=2, help="Enter the task id and the new description")
+parser.add_argument("--update", '-u', nargs=2, help="Enter the task id and the new description")
 parser.add_argument("--list","-l",nargs=1, help="List all the task")
 #parser.add_argument("--prueba","-p",help="Este argumento es para hacer pruebas")
 #parser.add_argument("delete")
@@ -40,7 +40,6 @@ def write_json(path_file,data):
     dic=read_json(path_file)
     size=len(list(dic))
     dic[str('task'+str(size+1))]=data
-    print(dic)
     with open(path_file,"w") as rewrite_file:
         json.dump(dic, rewrite_file, indent=4)     
 
@@ -48,27 +47,43 @@ def write_json(path_file,data):
 
 def add(descripcion):
     return descripcion
+def update(to_updte):
+    id_task=to_update[0]
+    new_description=to_update[1]
+
+    dic=read_json(paths[0])
+
+
 
 if __name__=='__main__':
 
     args=parser.parse_args()
     print(args)
-    if args.add:
-        task_id+=1
-        description=add(args.add)
-        status='todo'
-        created_at=getTime()
-    task={"task_id":task_id, "description":description, "status":status, "created_at":created_at, "updated_at":updated_at}
-    print("Task added successfully"+"("+"ID: "+str(task["task_id"])+")")
-
-    #check fi the all_tasks.json file exist
+    #check if the all_tasks.json file exist
     if not os.path.exists(paths[0]):
         with open(paths[0],'w') as all_tasks_file:
             json.dump({},all_tasks_file)
-
-    write_json(paths[0],task)
+    #Add
+    if args.add:
+        all_tasks_len=len(list(read_json(paths[0])))
+        task_id=all_tasks_len+1
+        description=add(args.add)
+        status='todo'
+        created_at=getTime()
+        #Creating the task dict
+        task={
+            "task_id":task_id, 
+            "description":description, 
+            "status":status, 
+            "created_at":created_at, 
+            "updated_at":updated_at
+        }
     
-
+        write_json(paths[0],task)
+        print("Task added successfully"+"("+"ID: "+str(task["task_id"])+")")
     
+    if args.update:
+        to_update=update(args.update)
+        print(to_update)
 
 
